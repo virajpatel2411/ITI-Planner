c9a5fa16d3f1a89b0dfb450dd1d5360b0437f6d9
unknown <virajpatel2411@gmail.com>
2020-02-19 19:35:46 +0530

    Initial Commit

---
+++ app/src/main/java/royal/com/itiplanner/MainActivity.java
@@ -0,0 +1,14 @@
+package royal.com.itiplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
