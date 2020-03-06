a2481f7dbd5d3272eef50a3c6c04b70b829217f0
unknown <virajpatel2411@gmail.com>
2020-03-06 17:24:15 +0530

    Google Sign In added

--- app/src/main/java/royal/com/itiplanner/MainActivity.java
+++ app/src/main/java/royal/com/itiplanner/MainActivity.java
@@ -1,39 +1,50 @@
 package royal.com.itiplanner;
 
 import android.content.Intent;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import android.support.annotation.NonNull;
-
-
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtEmail,edtPass;
-    Button btnSn,btnSu;
+    EditText edtEmail, edtPass;
+    Button btnSn, btnSu;
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    private String TAG = MainActivity.class.getName();
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +55,33 @@ public class MainActivity extends AppCompatActivity {
         btnSn = findViewById(R.id.btn_sn);
         btnSu = findViewById(R.id.btn_su);
 
+        signInButton = (SignInButton) findViewById(R.id.google_sn);
+
         mAuth = FirebaseAuth.getInstance();
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         myRef = database.getReference("Users");
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getResources().getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        /* mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions).build();*/
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+                // Intent signInIntent  = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 10);
+
+            }
+        });
 
 
         btnSu.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         btnSn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                while(true) {
+                while (true) {
                     if (TextUtils.isEmpty(edtEmail.getText().toString()) || !Patterns.EMAIL_ADDRESS.matcher(edtEmail.getText().toString()).matches()) {
                         edtEmail.setError("Please enter a valid Email ID!");
                         break;
@@ -86,11 +120,10 @@ public class MainActivity extends AppCompatActivity {
                             !Pattern.matches("[0-9]+$", edtPass.getText().toString()) &&
                             !Pattern.matches("[a-zA-Z]+$", edtPass.getText().toString())
                     ) {
-                        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtPass.getText().toString()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPass.getText().toString()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful())
-                                {
+                                if (task.isSuccessful()) {
                                     String strUId = mAuth.getUid();
 
                                     myRef.child(strUId).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -100,15 +133,14 @@ public class MainActivity extends AppCompatActivity {
 
                                             UserModel userModel = dataSnapshot.getValue(UserModel.class);
 
-                                            String strName  = userModel.getName();
-                                            String strId  = mAuth.getUid();
+                                            String strName = userModel.getName();
+                                            String strId = mAuth.getUid();
 
 
-
-                                            SharedPreferences sharedPreferences = getSharedPreferences("ITIPlanner",MODE_PRIVATE);
+                                            SharedPreferences sharedPreferences = getSharedPreferences("ITIPlanner", MODE_PRIVATE);
                                             SharedPreferences.Editor editor = sharedPreferences.edit();
-                                            editor.putString("NAME_KEY",strName);
-                                            editor.putString("UID_KEY",strId);
+                                            editor.putString("NAME_KEY", strName);
+                                            editor.putString("UID_KEY", strId);
 
                                             editor.commit();
 
@@ -127,9 +159,7 @@ public class MainActivity extends AppCompatActivity {
                                     });
 
 
-                                }
-                                else
-                                {
+                                } else {
                                     Toast.makeText(MainActivity.this, "Login failed. Incorrect details.", Toast.LENGTH_SHORT).show();
                                 }
                             }
@@ -141,4 +171,68 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 10) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount googleSignInAccount = task.getResult(ApiException.class);
+                firebasebaseLogin(googleSignInAccount);
+
+            } catch (ApiException e) {
+
+            }
+        }
+    }
+
+    private void firebasebaseLogin(GoogleSignInAccount googleSignInAccount) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String key = mAuth.getUid();
+
+                            SharedPreferences sharedPreferences = getSharedPreferences("ITIPlanner", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("NAME_KEY", user.getDisplayName());
+                            editor.putString("UID_KEY", key);
+                            editor.putString("GOOGLE_KEY", "GOOGLE");
+
+
+                            editor.commit();
+
+
+                            //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+
+                            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
 }
--- app/src/main/java/royal/com/itiplanner/ProfileFragment.java
+++ app/src/main/java/royal/com/itiplanner/ProfileFragment.java
@@ -12,38 +12,86 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 import static android.content.Context.MODE_PRIVATE;
 
-public class ProfileFragment extends Fragment
-{
+public class ProfileFragment extends Fragment {
     ListView listView;
     Button btnLogout;
-    String[] str = {"Personal Details","History","Feedback","About Us","Settings"};
-    int[] img = {R.drawable.ic_account,R.drawable.ic_history,R.drawable.ic_feedback,R.drawable.ic_help,R.drawable.ic_settings};
+    String[] str = {"Personal Details", "History", "Feedback", "About Us", "Settings"};
+    int[] img = {R.drawable.ic_account, R.drawable.ic_history, R.drawable.ic_feedback, R.drawable.ic_help, R.drawable.ic_settings};
+    private FirebaseAuth mAuth;
+    private String strGoogle;
+    private GoogleSignInClient mGoogleSignInClient;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_profile,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
         listView = rootView.findViewById(R.id.list);
-        MyListAdapter listAdapter = new MyListAdapter(getContext(),str,img);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("ITIPlanner", MODE_PRIVATE);
+        strGoogle = sharedPreferences.getString("GOOGLE_KEY", "");
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getResources().getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(),googleSignInOptions);
+
+       // mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), googleSignInOptions);
+        MyListAdapter listAdapter = new MyListAdapter(getContext(), str, img);
         listView.setAdapter(listAdapter);
         btnLogout = rootView.findViewById(R.id.btn_lo);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("ITIPlanner",MODE_PRIVATE);
-                String uId = sharedPreferences.getString("UID_KEY","");
-                if(!uId.equals(""))
-                {
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("ITIPlanner", MODE_PRIVATE);
+                String uId = sharedPreferences.getString("UID_KEY", "");
+                if (!uId.equals("")) {
                     SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("NAME_KEY","");
-                    editor.putString("UID_KEY","");
+                    editor.putString("NAME_KEY", "");
+                    editor.putString("UID_KEY", "");
 
                     editor.commit();
                 }
-                Intent intent = new Intent(getContext(),MainActivity.class);
-                startActivity(intent);
-                getActivity().finish();
+                if (strGoogle.equals("GOOGLE")){
+
+                    mAuth.signOut();
+
+                    // Google sign out
+                    mGoogleSignInClient.signOut().addOnCompleteListener(getActivity(),
+                            new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                                    startActivity(intent);
+                                    getActivity().finish();
+
+                                }
+                            });
+                }else {
+
+
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+
+
+                }
+
+
+
+
             }
 
         });
