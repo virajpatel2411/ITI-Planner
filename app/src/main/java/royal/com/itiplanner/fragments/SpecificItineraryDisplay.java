d3422f67c6c15e757022a2854341ee7d42a7d0d7
unknown <virajpatel2411@gmail.com>
2020-04-06 12:12:06 +0530

    Itinerary display modified, made compatible for small display devices

--- app/src/main/java/royal/com/itiplanner/fragments/SpecificItineraryDisplay.java
+++ app/src/main/java/royal/com/itiplanner/fragments/SpecificItineraryDisplay.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -20,6 +22,7 @@ public class SpecificItineraryDisplay extends Fragment {
   ListView listView;
   TextView txtDays,txtBudget;
   ArrayList<String> days,places,prices;
+  NestedScrollView scrollView;
   @Nullable @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
@@ -30,7 +33,7 @@ public class SpecificItineraryDisplay extends Fragment {
     String city = getArguments().getString("CITY");
 
     listView = rootView.findViewById(R.id.spec_rec);
-
+    scrollView = rootView.findViewById(R.id.scroll);
     txtBudget = rootView.findViewById(R.id.spec_budget);
     txtDays = rootView.findViewById(R.id.spec_days);
 
@@ -64,6 +67,20 @@ public class SpecificItineraryDisplay extends Fragment {
         displayItineraryAdapter = new DisplayItineraryAdapter(getContext(),days,places,prices);
 
     listView.setAdapter(displayItineraryAdapter);
+
+
+    listView.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View v, MotionEvent event) {
+        scrollView.requestDisallowInterceptTouchEvent(true);
+        int action = event.getActionMasked();
+        switch (action){
+          case MotionEvent.ACTION_UP:
+            scrollView.requestDisallowInterceptTouchEvent(false);
+            break;
+        }
+        return false;
+      }
+    });
 
     return rootView;
   }
